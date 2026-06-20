b9013d9b71654c155b5bcaa526ca5dd34b6f778d
Victor Barba <vbarba@local>
2026-06-20 22:10:15 +0200

    feat(ui): default "level time includes travel" to on

--- js/ui/app.js
+++ js/ui/app.js
@@ -97,7 +97,7 @@
       ascentRate: 9,
       lastStopDepth: 6,
       ppO2MaxDeco: 1.61,
-      segmentTimesIncludeTravel: false,
+      segmentTimesIncludeTravel: true,
       sacBottom: 20,
       sacDeco: 16,
       gasReserve: 'mingas',
